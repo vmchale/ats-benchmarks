a8d073da9d667a6adf878a97438f819361583e5f
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-01-09 04:18:49 -0600

    property test

--- hs/test/Spec.hs
+++ hs/test/Spec.hs
@@ -2,6 +2,9 @@ import           Lib
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 
+factorial :: Int -> Integer
+factorial n = product [1..(fromIntegral n)]
+
 main :: IO ()
 main = hspec $ do
     parallel $ describe "collatzATS" $
@@ -10,6 +13,9 @@ main = hspec $ do
     parallel $ describe "collatzC" $
         prop "should agree with the pure Haskell function" $
             \x -> x < 1 || collatzC x == collatzPure x
+    parallel $ describe "derangement" $
+        prop "should agree with the appropriate formula" $
+            \n -> n < 1 || n > 18 || derangement n == floor ((fromIntegral (factorial n) :: Double) / (exp 1) + 0.5)
     parallel $ describe "derangementATS" $
         it "should agree with the pure Haskell function" $
             derangementATS 35 >>= (`shouldBe` derangement 35)
